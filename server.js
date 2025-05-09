2b93a37f3e7f92ff79abbb2ccfc433722bfc06d0
U2iiii <tomopowan@gmail.com>
2025-05-09 11:14:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const fetch = require('node-fetch');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
